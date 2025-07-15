b891574eda8469fca484cab820deb288ebfc9dbd
sabian77 <sabianrakasinau@gmail.com>
2025-07-15 10:47:35 +0700

    menambahkan login bisa pakai username

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -25,6 +25,14 @@ class ProfileUpdateRequest extends FormRequest
                 'max:255',
                 Rule::unique(User::class)->ignore($this->user()->id),
             ],
+            'username' => [
+                'required',
+                'string',
+                'min:3',
+                'max:18',
+                'alpha_num',
+                Rule::unique(User::class)->ignore($this->user()->id),
+            ],
         ];
     }
 }
